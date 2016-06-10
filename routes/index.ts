f458e321f82947d45fce8f51ecd4b901df28f91b
Derek P Sifford <dereksifford@gmail.com>
2016-06-09 21:28:26 -0400

    Add newsubmissions pipeline

--- routes/index.ts
+++ routes/index.ts
@@ -1,8 +1,10 @@
 import * as express from 'express';
 import aliemu from './aliemu/';
+import newsubmissions from './newsubmissions/';
 const router = express.Router();
 
 export default function route(bot: Botkit.Bot): express.Router {
     router.use('/aliemu', aliemu(bot));
+    router.use('/newsubmissions', newsubmissions(bot));
     return router;
 };
---
+++ routes/newsubmissions/hooks.ts
@@ -0,0 +1,47 @@
+import * as express from 'express';
+const router = express.Router();
+const CHANNEL_ID = 'G0KM9J46P';
+
+export default function route(bot: Botkit.Bot): express.Router {
+
+    router.post('/draft-submit', (req, res) => {
+        bot.say({
+            channel: CHANNEL_ID,
+            attachments: [
+                {
+                    fallback: `New Submission Received: ${req.body.title}. Copyeditor assigned: <@${req.body.copyeditor}>. URL: ${req.body.draftUrl}`,
+                    color: '#00B092',
+                    title: 'New Submission Received',
+                    fields: [
+                        {
+                            title: 'Title',
+                            value: `${req.body.title}`,
+                            short: true,
+                        },
+                        {
+                            title: 'First Author',
+                            value: `${req.body.author}`,
+                            short: true,
+                        },
+                        {
+                            title: 'Copyeditor Assignment',
+                            value: `<@${req.body.copyeditor}>`,
+                            short: true,
+                        },
+                        {
+                            title: 'URLs',
+                            value: `<https://www.aliem.com/wp-admin/|Login Page> | <${req.body.draftUrl}|Draft Page>`,
+                            short: true,
+                        },
+                    ],
+                    footer: 'Note: The draft URL will not work unless you are logged in.',
+                },
+            ],
+        } as Botkit.MessageWithoutContext, (err, resp) => {
+            if (err) return res.sendStatus(503);
+            res.sendStatus(200);
+        });
+    });
+
+    return router;
+}
