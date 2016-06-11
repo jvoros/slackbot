ac81a1ef6dc7d2275f688a15b75acbb603b89ec0
Derek P Sifford <dereksifford@gmail.com>
2016-06-11 19:13:42 -0400

    Add ALiEMU contact page integration

--- routes/aliemu/index.ts
+++ routes/aliemu/index.ts
@@ -1,10 +1,12 @@
 import * as express from 'express';
 import hooks from './hooks';
+import messages from './messages';
 const router = express.Router();
 
 export default function route(bot: Botkit.Bot): express.Router {
 
     router.use('/hooks', hooks(bot));
+    router.use('/messages', messages(bot));
 
     router.get('/', (req, res) => {
         res.send('Invalid endpoint.');
---
+++ routes/aliemu/messages.ts
@@ -0,0 +1,42 @@
+import * as express from 'express';
+const router = express.Router();
+const CHANNEL_ID = 'C09762GTV'; // #aliemu
+
+export default function route(bot: Botkit.Bot): express.Router {
+
+    router.post('/contact-form', (req, res) => {
+        const { name, email, message } = req.body.data;
+        if (!name || !email || !message) return res.sendStatus(400);
+        bot.say({
+            channel: CHANNEL_ID,
+            attachments: [
+                {
+                    fallback: `Contact form message from ${name}: ${message} -- Email: {email}`,
+                    title: 'Message Received from Contact Us Form',
+                    fields: [
+                        {
+                            title: 'From',
+                            value: `${name}`,
+                            short: true,
+                        },
+                        {
+                            title: 'Email Address',
+                            value: `${email}`,
+                            short: true,
+                        },
+                        {
+                            title: 'Message',
+                            value: `${message}`,
+                            short: false,
+                        },
+                    ],
+                },
+            ],
+        } as Botkit.MessageWithoutContext, (err, resp) => {
+            if (err) return res.sendStatus(503);
+            res.sendStatus(200);
+        });
+    });
+
+    return router;
+}
