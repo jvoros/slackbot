1282dd0e31de8730e781c4316507c21b7cad974d
Derek P Sifford <dereksifford@gmail.com>
2016-06-24 17:32:24 -0400

    Add debug logs

--- helpers/authentication.ts
+++ helpers/authentication.ts
@@ -6,12 +6,14 @@ const SERVER_SECURED = false;
 export function requireAuthentication(req, res, next) {
     /** FIXME */
     console.log('============HEADERS============');
-    console.log(req.headers);
-    console.log('=> Lowercase Key: ', req.header('aliem_api_key'));
-    console.log('=> Uppercase Key: ', req.header('ALIEM_API_KEY'));
+    console.log(req.get('ALIEM_API_KEY'));
+    console.log(req.get('aliem_api_key'));
+    console.log(req.get('AlIEm_aPI_keY'));
+    console.log('Is JSON = ', req.is('json'));
+    console.log('Is urlencoded = ', req.is('urlencoded'));
     console.log('=============BODY==============');
     console.log(req.body);
-    if (req.header('aliem_api_key') !== process.env.ALIEM_API_KEY && SERVER_SECURED) {
+    if (req.get('ALIEM_API_KEY') !== process.env.ALIEM_API_KEY && SERVER_SECURED) {
         res.status(401).send('=> ERROR: "aliem_api_key" header not set or invalid');
         return;
     }
