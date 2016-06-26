d70b176f537e09497cad6237fcdb41b89ca6bf37
Derek P Sifford <dereksifford@gmail.com>
2016-06-25 22:56:34 -0400

    Enable authentication middleware

--- helpers/authentication.ts
+++ helpers/authentication.ts
@@ -1,10 +1,7 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
-/** FIXME: Remove this when ready to implement */
-const SERVER_SECURED = false;
-
 export function requireAuthentication(req, res, next) {
-    if (req.get('ALIEM_API_KEY') !== process.env.ALIEM_API_KEY && SERVER_SECURED) {
+    if (req.get('ALIEM_API_KEY') !== process.env.ALIEM_API_KEY) {
         res.status(401).send('=> ERROR: "aliem_api_key" header not set or invalid');
         return;
     }
